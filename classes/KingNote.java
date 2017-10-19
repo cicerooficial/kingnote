85713a23c09e357ee6ee0a31dd9dcdd50e23a996
Victor Rubens <victorrss@live.com>
2017-10-19 10:01:48 -0200

    remove old

--- classes/KingNote.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class KingNote {
-	private int qtdNotes = 0;
-	private ArrayList<Note> notes = new ArrayList<Note>();
-	private ArrayList<Note> trash = new ArrayList<Note>();
-	
-	
-}
